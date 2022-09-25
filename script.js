749d8d6eeac04c1a97617c737efdbe927726d789
Shahed Arzu <sdaurn@gmail.com>
2022-09-25 16:23:07 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        autoplay:true,
+        autoplayTimeout: 3000,
+        loop:true,
+        nav:true,
+        dots:true,
+        navText: ['prev','next']
+        
+    });
+  });
